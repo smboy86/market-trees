071ade87b6b0acb9fafc1f9b43c122c449a4cacf
devpoi <smboy86@naver.com>
2024-06-19 17:19:05 +0900

    mod - 샘플 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 
+import { Container } from "@/components/layout/container";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={() => alert("안녕하세요, 마켓 연리지 입니다.")}>
-        Hi
-      </Button>
+      View - Navigatator
+      <Container variant={"narrowConstrainedPadded"}>
+        <nav>
+          <Button onClick={() => alert("안녕하세요, 마켓 연리지 입니다.")}>
+            Hi
+          </Button>
+        </nav>
+      </Container>
+      View - Footer
     </main>
   );
 }
