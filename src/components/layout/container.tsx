b930eab69d4c22ea499301145436e62d00dd5c68
devpoi <smboy86@naver.com>
2024-06-20 13:21:45 +0900

    dev - supabase DB 연결

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -1,20 +1,20 @@
-import * as React from "react";
-import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
 
-const containerVariants = cva("mx-auto px-4 sm:px-6 lg:px-8", {
+const containerVariants = cva('mx-auto px-4 sm:px-6 lg:px-8', {
   variants: {
     variant: {
-      fullMobileConstrainedPadded: "max-w-7xl sm:px-6 lg:px-8",
-      constrainedPadded: "max-w-7xl px-4 sm:px-6 lg:px-8",
-      fullMobileBreakpointPadded: "container mx-auto sm:px-6 lg:px-8",
-      breakpointPadded: "container mx-auto px-4 sm:px-6 lg:px-8",
-      narrowConstrainedPadded: "max-w-7xl px-4 sm:px-6 lg:px-8 max-w-3xl",
-      pcLayout: "min-w-7xl px-4",
+      fullMobileConstrainedPadded: 'max-w-7xl sm:px-6 lg:px-8',
+      constrainedPadded: 'max-w-7xl px-4 sm:px-6 lg:px-8',
+      fullMobileBreakpointPadded: 'container mx-auto sm:px-6 lg:px-8',
+      breakpointPadded: 'container mx-auto px-4 sm:px-6 lg:px-8',
+      narrowConstrainedPadded: 'max-w-7xl px-4 sm:px-6 lg:px-8 max-w-3xl',
+      pcLayout: 'min-w-7xl px-4',
     },
   },
   defaultVariants: {
-    variant: "narrowConstrainedPadded",
+    variant: 'pcLayout',
   },
 });
 
@@ -31,13 +31,15 @@ const Container: React.FC<ContainerProps> = ({
   variant,
   ...props
 }) => {
-  const Comp = asChild ? React.Fragment : "div";
+  const Comp = asChild ? React.Fragment : 'div';
   const containerClasses = cn(containerVariants({ variant }), className);
 
   return (
     <Comp className={containerClasses} {...props}>
-      {variant === "narrowConstrainedPadded" ? (
+      {variant === 'narrowConstrainedPadded' ? (
         <div className="mx-auto max-w-3xl">{children}</div>
+      ) : variant === 'pcLayout' ? (
+        <div className="mx-auto max-w-[1080px] min-w-[1080px]">{children}</div>
       ) : (
         children
       )}
