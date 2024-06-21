4e513f4a6de17d91765581da3710a24db63739a1
devpoi <smboy86@naver.com>
2024-06-22 00:01:45 +0900

    mod - warning ignore

--- src/components/auth/checkUserSession.ts
+++ src/components/auth/checkUserSession.ts
@@ -53,6 +53,7 @@ export default function CheckUserSession() {
     return () => {
       channel.data.subscription.unsubscribe();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [supabase]);
 
   return null;
