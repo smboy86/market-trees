586b024976ee6fa29517ff717b1d8936249d757c
devpoi <smboy86@naver.com>
2024-06-22 00:29:11 +0900

    dev - 메인 슬라이더

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,50 @@
 'use client';
 
-import CheckUserSession from '@/components/auth/checkUserSession';
 import { Container } from '@/components/layout/container';
 import Wrap from '@/components/layout/wrap';
-import { Button } from '@/components/ui/button';
-import { createClient } from '@/utils/supabase/client';
-import { useUser } from '@supabase/auth-helpers-react';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 export default function App() {
   return (
     <Container>
       <Wrap>
-        <div className="">1) 슬라이더 영역</div>
-        <div className="">2) 캘런디 영역</div>
-        <div className="">3) 지도 영역</div>
+        <div className="w-full mb-10">
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-center">
+            마켓 리뷰
+          </h4>
+          <Carousel className="w-full mt-1">
+            <CarouselContent className="-ml-1">
+              {Array.from({ length: 10 }).map((_, index) => (
+                <CarouselItem key={index} className="pl-1 basis-1/5">
+                  <div className="p-1">
+                    <Card
+                      onClick={() => alert(`마켓 ${index + 1}`)}
+                      className="cursor-pointer"
+                    >
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-2xl font-semibold">
+                          Img {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+
+        <div className="w-full border text-center mb-10">2) 캘린더 영역</div>
+        <div className="w-full border text-center">3) 지도 영역</div>
       </Wrap>
     </Container>
   );
