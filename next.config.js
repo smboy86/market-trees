6caceb03ba7d5b6abbedb6ef7978c5f86772caa0
devpoi <smboy86@naver.com>
2024-07-17 11:20:54 +0900

    mod - config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['k.kakaocdn.net'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'k.kakaocdn.net',
+      },
+    ],
   },
 };
 
